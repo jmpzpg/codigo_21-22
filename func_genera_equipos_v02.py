f3f1d5cb658eb09dce34be63d9fcdce9be4b7ca7
jmpzpg <jmpzpg@gmail.com>
2021-10-27 09:11:08 +0200

    Nuevo Ejercicio

--- func_genera_equipos_v02.py
+++ func_genera_equipos_v02.py
@@ -3,14 +3,20 @@ import random
 def func_genera_equipos_v02(alumnos, miembros):
     random.shuffle(alumnos)
     equipos = []
+    num_equipos = len(alumnos) // 2
+    pos_final_parejas = 0
 
     for i in range(0,len(alumnos),miembros):
         equipos.append(alumnos[i:i+miembros])
 
-    return equipos
+    
 
+    if ( len(alumnos) % miembros != 0 ):
+        equipos[num_equipos - 1].append(alumnos[len(alumnos)-1:])  # linea inacabada
+
+    return equipos
 
 gente = [1,2,3,4,5,6,7,8,9]
 for x in range(5):
-    x = func_genera_equipos_v02(gente,3)
+    x = func_genera_equipos_v02(gente,4)
     print(x)
---
+++ matriz20x20.py
@@ -0,0 +1,5 @@
+columna = [('x'*20)]
+
+for i in range(20):
+    x = columna
+    print(x)
