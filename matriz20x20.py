98aef3fa398ee4b06135ddd71cfad0e6a910ea31
jmpzpg <jmpzpg@gmail.com>
2021-10-27 09:33:45 +0200

    cambios en Matriz20x20 para dejarla como al principio

--- matriz20x20.py
+++ matriz20x20.py
@@ -1,22 +1,10 @@
 
-import pprint
-cols = 15
-filas = 15
-matriz = []
 
-for i in range(filas):
-    fila = []
-    for j in range(cols):
-        fila.append('X')
-    matriz.append(fila)
-pprint.pprint(matriz)
-
-
-""" fila = [('x'*20)]
+fila = [('x'*20)]
 
 for i in range(20):
     x = fila
-    print(x) """
+    print(x)
 
 
 """ fila = []
