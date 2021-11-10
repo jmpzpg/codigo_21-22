54bea3b8bfd77dcc8bb55d2d50b75b707670f82b
jmpzpg <jmpzpg@gmail.com>
2021-11-10 12:59:37 +0100

    primera kata en clase

--- Diccionarios/archivos_a_diccionario.py
+++ Diccionarios/archivos_a_diccionario.py
@@ -3,20 +3,16 @@ import os
 import pprint
 from config_rutas import RUTA_BASE, RUTA_CODIGO, MI_CARPETA
 
-#nuevo_dir = os.chdir('/')
-
-
 
 def clear():
     os.system('clear')
+
 
 # Primero hay que abrir y leer del archivo de entrada una fila y la troceamos para tomar cada nombre de archivo
 def func_leer_archivo(ruta, nom_archivo, modo_ap):
     lista_de_filas = []
     nuevo = open(ruta + nom_archivo, modo_ap)
-    
     salida_lectura_archivo = nuevo.readlines()
-    #lista_de_filas.append(salida_lectura_archivo)
     nuevo.close()
     return salida_lectura_archivo
 
@@ -35,13 +31,5 @@ for i in x:
         dic_salida[clave] = lista_de_fila[k]
         clave += 1
         
-
-
-""" for j in range(len(x)):
-    list_aux = x[j].split(',')
-    for i in range(len(list_aux)):
-        list_aux_2 = x[i].split(',')
-        dic_salida[(i+j)] = list_aux_2[i]
-         """
 
 pprint.pprint(dic_salida)
---
+++ Diccionarios/unique_in_order.py
@@ -0,0 +1,47 @@
+""" https://www.codewars.com/kata/54e6533c92449cc251001667/train/python
+
+Implement the function unique_in_order which takes as argument a sequence and 
+returns a list of items without any elements with the same value next to each other and 
+preserving the original order of elements.
+
+    unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+    unique_in_order([1,2,2,3,3])       == [1,2,3]
+
+"""
+
+def unique_in_order_total(iterable: str):
+    lista_salida = []
+    cadena_a_lista = list(iterable)
+    for i in cadena_a_lista:
+        if i not in lista_salida:
+            lista_salida.append(i)
+    return lista_salida
+
+def unique_in_order2(iterable: str):
+    cadena_a_lista = list(iterable)
+    lista_salida = []
+    if cadena_a_lista != []:
+        lista_salida.append(cadena_a_lista[0])
+        for i in range(len(cadena_a_lista)):
+            if i != 0:
+                if cadena_a_lista[i] != lista_salida[len(lista_salida)-1]:
+                    lista_salida.append(cadena_a_lista[i])
+    return lista_salida
+
+
+def unique_in_order(iterable: str):
+    cadena_a_lista = list(iterable)
+    lista_salida = []
+    if cadena_a_lista != []:
+        lista_salida.append(cadena_a_lista[0])
+        for i in cadena_a_lista:
+            if i != lista_salida[len(lista_salida)-1]:
+                lista_salida.append(i)
+    return lista_salida
+
+
+
+y = unique_in_order_total('AABBcddAB')
+x = unique_in_order('AABBcddAB')
+print(y)
+print(x)
