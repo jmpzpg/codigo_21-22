a80a05bab3b88c092c7d495f46b0f5ef67cb667e
jmpzpg <jmpzpg@gmail.com>
2022-01-13 10:17:59 +0100

    actualizacion del conversor

--- Trimestre-2/12_convertir_a_ascii_MIO.py
+++ Trimestre-2/12_convertir_a_ascii_MIO.py
@@ -1,4 +1,4 @@
-# Problema:
+# Ejercicio Pagina-45:
 """
 Dada una cadena de texto, convertir cada carcater a su codigo ASCII.
 Con eso creamos un numero formado por todos los codigos unidos, que llamaremos total_1.
@@ -6,18 +6,16 @@ Cambiar todos los numeros '7' por el numero '1', al que llamaremos total_2
 Devolver la resta de total_1 - total_2
 
 """
-
+ 
 
 def convertir_a_ascii_y_restarlos(cadena):
     total_1 = total_2 = ''
 
     for elem in cadena:
         total_1 += str(ord(elem))
-
-    print(total_1)
-
     total_2 = total_1.replace('7','1')
     
+    print(total_1)
     print(total_2)
 
     return int(total_1) - int(total_2)
