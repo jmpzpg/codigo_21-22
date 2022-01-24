b3b814e3c6a1c03f2b43916287b85503dee6d480
jmpzpg <jmpzpg@gmail.com>
2022-01-24 10:53:04 +0100

    seguimos con Calificaciones en clase

--- Trimestre-2/Ejemplo_Calificaciones/metodo_estatico.py
+++ Trimestre-2/Ejemplo_Calificaciones/metodo_estatico.py
@@ -2,23 +2,23 @@ class Calificaciones():
     def __init__(self, alumno_lista=[]) -> None:
         
         if alumno_lista:
-            self.nombre = alumno_lista[0]
-            self.notas = alumno_lista[1:]
-            self.calificacion = self.calcula_calificacion()
+            self.__nombre = alumno_lista[0]
+            self.__notas = alumno_lista[1:]
+            self.__calificacion = self.calcula_calificacion()
         else:
-            self.nombre = ''
-            self.notas = []
-            self.calificacion = ''
+            self.__nombre = ''
+            self.__notas = []
+            self.__calificacion = ''
             
 
     def __str__(self) -> str:
-        return f'Alumno: {self.nombre} tiene la calificación: {self.calificacion}'
+        return f'Alumno: {self.__nombre} tiene la calificación: {self.calificacion}'
         
-        
+
     def calcula_calificacion(self):
         salida = ''
-        if self.notas:
-            nota_media = sum(self.notas) / len(self.notas)
+        if self.__notas:
+            nota_media = sum(self.__notas) / len(self.__notas)
             if 0 < nota_media < 4.9:
                 salida = 'SUSPENSO'
             elif 5 <= nota_media < 5.5:
@@ -34,3 +34,44 @@ class Calificaciones():
         else:
             salida = None
         return salida
+
+    def set_alumno(self, alum):
+        self.__nombre = alum[0]
+        for elem in alum[1:]:
+            self.__notas.append(elem)
+        self.__calificacion = self.calcula_calificacion()
+    
+    @property
+    def alumno(self):
+        return self.__nombre
+    @alumno.setter
+    def alumno(self, nuevo_alum):
+        self.__nombre = nuevo_alum
+
+    @property
+    def notas(self):
+        return self.__notas
+    @notas.setter
+    def notas(self, nuevas_notas):
+        if self.valida_notas(nuevas_notas):
+            self.__notas = nuevas_notas
+            self.__calificacion = self.calcula_calificacion()
+        else:
+            raise Exception('Notas no válidas')
+
+    @property
+    def calificacion(self):
+        return self.__calificacion
+
+    @staticmethod
+    def valida_notas(lista_notas):
+        # verdadero si son validas (float entre 0 y 10) y false si no lo son
+        # Falso para tipo distinto a int o float o lista vacia
+        valido = True
+        if not lista_notas:
+            valido = False
+        else:
+            for elem in lista_notas:
+                if not type(elem) in (int,float) or not 0.0 <= elem <= 10.0:
+                    valido = False
+        return valido
--- Trimestre-2/Ejemplo_Calificaciones/test_calificador.py
+++ Trimestre-2/Ejemplo_Calificaciones/test_calificador.py
@@ -8,13 +8,13 @@ class TestClaseCalificaciones(unittest.TestCase):
 
     def test_constructor_x_defecto_nombre_cadvacia_notas_listavacia_calificacion_cadvacia(self):
         c = Calificaciones()
-        self.assertEqual(c.nombre, '')
+        self.assertEqual(c.alumno, '')
         self.assertEqual(c.notas, [])
         self.assertEqual(c.calificacion, '')
 
     def test_constructor_asigna_valor_propiedades(self):
         c = Calificaciones(['Raul', 9.2, 5, 4.5, 7, 9.1])
-        self.assertEqual(c.nombre, 'Raul')
+        self.assertEqual(c.alumno, 'Raul')
         self.assertEqual(c.notas, [9.2, 5, 4.5, 7, 9.1])
 
     def test_metodo_str_dev_alumno_calificacion(self):
@@ -30,3 +30,38 @@ class TestClaseCalificaciones(unittest.TestCase):
         c = Calificaciones(['Raul', 9.2, 5, 4.5, 7, 9.1])
         self.assertEqual(c.calcula_calificacion(), 'NOTABLE') 
         self.assertEqual(c.calificacion, 'NOTABLE')
+
+    def test_set_alumno_vacio_actualiza_nombre_y_notas(self):
+        c = Calificaciones()
+        c.set_alumno(['Pedro', 9.5])
+        self.assertEqual(c.alumno, 'Pedro')
+        self.assertEqual(c.notas, [9.5])
+        self.assertEqual(c.calificacion, 'EXCELENTE') 
+
+    def test_asigna_valores_alumno_vacio(self):
+        c = Calificaciones()
+        c.nombre = 'Paco' 
+        self.assertEqual(c.nombre, 'Paco')
+
+
+    def test_asigna_notas(self):
+        c = Calificaciones()
+        c.notas = [5,6,7,8,9,10]
+        self.assertEqual(c.notas, [5,6,7,8,9,10])
+        self.assertEqual(c.calificacion, 'NOTABLE') 
+
+    def test_validar_notas_incorrectas(self):
+        notas_a_validar = [11]
+        self.assertEqual(Calificaciones.valida_notas(notas_a_validar), False)
+
+    def test_validar_notas_incorrectas_cadena(self):
+        notas_a_validar = [10, 'hola']
+        self.assertEqual(Calificaciones.valida_notas(notas_a_validar), False)
+    
+    def test_validar_notas_incorrectas_lista_vacia(self):
+        notas_a_validar = []
+        self.assertEqual(Calificaciones.valida_notas(notas_a_validar), False)
+
+    def test_validar_notas_correctas(self):
+        notas_a_validar = [5.3]
+        self.assertEqual(Calificaciones.valida_notas(notas_a_validar), True)
