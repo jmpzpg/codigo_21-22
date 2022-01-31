c1ed238bd3835672d666c3667216ca4e707f8934
jmpzpg <92719993+jmpzpg@users.noreply.github.com>
2022-01-31 20:26:07 +0100

    .

--- Trimestre-2/Ejercicio_Nif/test_Nif.py
+++ Trimestre-2/Ejercicio_Nif/test_Nif.py
@@ -1,17 +1,12 @@
-import pstats
 import unittest
-from clase_Nif import Nif
+from clase_NIF import Nif 
 
 class TestNif(unittest.TestCase):
     def test_clase_existe(self):
-        nif = Nif()
-        self.assertNotEqual(nif,None)
+        resp = Nif()
+        self.assertNotEqual(resp,None)
 
     def test_constructor_x_defecto_num_cero_y_letra_blanco(self):
-        nif = Nif()
-        self.assertEqual(nif.numero, 0)
-        self.assertEqual(nif.letra, ' ')
-
-    def test_numero_calcula_letra(self):
-        nif = Nif(123456)
-        self.assertEqual(nif.letra, 'S')
+        resp = Nif()
+        self.assertEqual(resp.numero, 0)
+    
